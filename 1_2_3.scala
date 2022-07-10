e5a1338a3f59ac38510307a23a771662a6326eef
madhawaUCSC2453 <89871046+madhawaucsc2453@users.noreply.github.com>
2022-07-10 21:57:40 +0530

    Add files via upload

---
+++ 1_2_3.scala
@@ -0,0 +1,20 @@
+
+val k,i,j=2;
+val m,n=2;
+val f=12.0f;
+val g=4.0f;
+val c='X';
+
+println(k+12*m);
+println(m/j);
+println(n%j);
+println(m/j*j);
+println(f+10*5+g);
+
+val a=2;
+val b=3;
+val c=4;
+val d=5;
+val k=4.3f ;
+
+println(-2*(g-k)+c);
